48d3f92abefffecd27c1054e556337f93f7b605c
Igor Gorbanev <gorbanev_i@mail.ru>
2026-04-24 20:39:52 +0300

    Deletetodolist thunk

--- src/features/todolists/model/todolists-slice.ts
+++ src/features/todolists/model/todolists-slice.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit"
 import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts"
 import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
 
-export type DomainTodolist=Todolist & {
+export type DomainTodolist = Todolist & {
   filter: FilterValues
 }
 
@@ -16,12 +16,7 @@ export const todolistsSlice = createSlice({
         state.splice(index, 1)
       }
     }),
-    changeTodolistTitleAC: create.reducer<{ id: string; title: string }>((state, action) => {
-      const index = state.findIndex((todolist) => todolist.id === action.payload.id)
-      if (index !== -1) {
-        state[index].title = action.payload.title
-      }
-    }),
+
     changeTodolistFilterAC: create.reducer<{ id: string; filter: FilterValues }>((state, action) => {
       const index = state.findIndex((todolist) => todolist.id === action.payload.id)
       if (index !== -1) {
@@ -31,36 +26,68 @@ export const todolistsSlice = createSlice({
     createTodolistAC: create.preparedReducer(
       (title: string) => ({ payload: { title, id: nanoid() } }),
       (state, action) => {
-        state.push({ ...action.payload, filter: "all",addedDate: '', order: 0 })
+        state.push({ ...action.payload, filter: "all", addedDate: "", order: 0 })
       },
     ),
-    setTodolistAC: create.reducer<{ todolists: Todolist[] }>((state, action)=>{
+    setTodolistAC: create.reducer<{ todolists: Todolist[] }>((state, action) => {
       return action.payload.todolists.map((tl) => {
-        return {...tl, filter:'all'}
+        return { ...tl, filter: "all" }
       })
     }),
-
   }),
-  extraReducers: builder => {
-
-  }
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodolistsTC.fulfilled, (state, action) => {
+        return action.payload.todolists.map((tl) => {
+          return { ...tl, filter: "all" }
+        })
+      })
+      .addCase(fetchTodolistsTC.rejected, (state, action) => {})
+      .addCase(changeTodolistTitleTC.fulfilled, (state, action) => {
+        return state.map((tl) => (tl.id === action.payload.id ? { ...tl, title: action.payload.title } : tl))
+      })
+      .addCase(changeTodolistTitleTC.rejected, (state, action) => {})
+      .addCase(deleteTodolistTC.fulfilled, (state, action)=>{
+        return state.filter(tl=>tl.id!==action.payload.id)
+      })
+  },
 })
 
-export const fetchTodolistsTC = createAsyncThunk(
-  `${todolistsSlice.name}/fetch-todolists`,
-    async (_,thunkAPI)=>{
+export const fetchTodolistsTC = createAsyncThunk(`${todolistsSlice.name}/fetch-todolists`, async (_, thunkAPI) => {
+  try {
+    const res = await todolistsApi.getTodolists()
+    return { todolists: res.data }
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error)
+  }
+})
+export const changeTodolistTitleTC = createAsyncThunk(
+  `${todolistsSlice.name}/change-todolist-title`,
+  async (payload: { id: string; title: string }, thunkAPI) => {
     try {
-      const res = await todolistsApi.getTodolists()
-      return {todolists: res.data}
+      await todolistsApi.changeTodolistTitle(payload)
+      return payload
     }
     catch (error){
       return thunkAPI.rejectWithValue(error)
     }
 
+  },
+)
+export const deleteTodolistTC = createAsyncThunk(
+  `${todolistsSlice.name}/delete-todolist`,
+  async (payload:{id:string},thunkAPI)=>{
+    try{
+      await todolistsApi.deleteTodolist(payload.id)
+      return payload
+    }
+    catch (error){
+      return thunkAPI.rejectWithValue(error)
+    }
   }
 )
 
-export const {setTodolistAC, deleteTodolistAC, changeTodolistTitleAC, changeTodolistFilterAC, createTodolistAC } = todolistsSlice.actions
-
+export const { setTodolistAC, deleteTodolistAC,  changeTodolistFilterAC, createTodolistAC } =
+  todolistsSlice.actions
 
 export type FilterValues = "all" | "active" | "completed"
--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -1,6 +1,12 @@
 import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan"
 import { useAppDispatch } from "@/common/hooks"
-import { changeTodolistTitleAC, deleteTodolistAC, type DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
+import {
+  changeTodolistTitleAC,
+  changeTodolistTitleTC,
+  deleteTodolistAC,
+  deleteTodolistTC,
+  type DomainTodolist,
+} from "@/features/todolists/model/todolists-slice.ts"
 import DeleteIcon from "@mui/icons-material/Delete"
 import IconButton from "@mui/material/IconButton"
 import styles from "./TodolistTitle.module.css"
@@ -15,11 +21,11 @@ export const TodolistTitle = ({ todolist }: Props) => {
   const dispatch = useAppDispatch()
 
   const deleteTodolist = () => {
-    dispatch(deleteTodolistAC({ id }))
+    dispatch(deleteTodolistTC({ id }))
   }
 
   const changeTodolistTitle = (title: string) => {
-    dispatch(changeTodolistTitleAC({ id, title }))
+    dispatch(changeTodolistTitleTC({ id, title }))
   }
 
   return (
